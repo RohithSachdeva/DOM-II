40a60a3037ee4fd462187381b9faba1ce2cf69e8
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-10 15:50:37 -0700

    Random Notes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+// // Your code goes here
+
+// homeLink.addEventListener(
+//     'click', //string w the name of the event
+//     () => {
+//         //things we want happening when this link is clicked
+//         // link gets clicked
+//         console.log('the link got clicked');
+//     }
+// )
+
+// document.querySelector('nav').addEventListener('click', () => {
+//     console.log('the nav got clicked');
+// })
+
+// document.querySelector('header').addEventListener('click', () => {
+//     console.log('the header got clicked');
+// })
+
+// document.body.addEventListener('click', () => {
+//     console.log('the body of the page got clicked');
+// })
+
+
+function clickEventHandler(event) {
+    console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  }
+  document.querySelector('a').addEventListener('click', clickEventHandler)
+  document.querySelector('nav').addEventListener('click', clickEventHandler)
+  document.querySelector('header').addEventListener('click', clickEventHandler)
+  document.body.addEventListener('click', clickEventHandler)
+  document.addEventListener('click', clickEventHandler)
+  window.addEventListener('click', clickEventHandler)
