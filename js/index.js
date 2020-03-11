140b5fcee11dbd48f785a51259173e632a4e1bf3
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-03-11 13:05:23 -0700

    Forgot to push

--- js/index.js
+++ js/index.js
@@ -22,12 +22,89 @@
 // })
 
 
-function clickEventHandler(event) {
-    console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  }
-  document.querySelector('a').addEventListener('click', clickEventHandler)
-  document.querySelector('nav').addEventListener('click', clickEventHandler)
-  document.querySelector('header').addEventListener('click', clickEventHandler)
-  document.body.addEventListener('click', clickEventHandler)
-  document.addEventListener('click', clickEventHandler)
-  window.addEventListener('click', clickEventHandler)
+//  function clickEventHandler(event) {
+//     console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//   }
+//   document.querySelector('a').addEventListener('click', clickEventHandler)
+//   document.querySelector('nav').addEventListener('click', clickEventHandler)
+//   document.querySelector('header').addEventListener('click', clickEventHandler)
+//   document.body.addEventListener('click', clickEventHandler)
+//   document.addEventListener('click', clickEventHandler)
+//   window.addEventListener('click', clickEventHandler)
+
+
+//1
+const navigationLinks = document.querySelector('a');
+console.log(navigationLinks);
+
+navigationLinks.addEventListener('click', () => {
+  console.log('clicked nav bar');
+  navigationLinks.style.color = 'red';
+});
+
+
+
+
+//2 3
+  const funBus = document.querySelector('.intro img');
+  funBus.addEventListener('mouseover', () => {
+    funBus.style.transform = 'scale(1.5)';
+  });
+
+  funBus.addEventListener('mouseleave', () => {
+    funBus.style.transform = 'scale(1)';
+  });
+
+// 4
+  const firstButton = document.querySelector('.btn');
+  firstButton.addEventListener('dblclick', () => {
+    firstButton.style.backgroundColor = 'red';
+  });
+
+
+
+//5
+  
+  const destinationImg = document.querySelector('.content-destination img');
+
+destinationImg.addEventListener('mousemove', function(){
+  destinationImg.style.opacity="0.7";
+  destinationImg.style.filter="alph(opacity=80)";
+});
+
+//6
+const contentSect = document.querySelector('.content-section') 
+contentSect.addEventListener('wheel', function(){
+  event.currentTarget.style.border = "5px solid blue"; 
+});
+
+//7 
+  const dragNav = document.querySelector('a');
+  dragNav.addEventListener('drag', () => {
+  dragNav.textContent = "Drag Me!";
+});
+
+//8  9
+const yellowBlurbs = document.querySelector('.destination')
+yellowBlurbs.addEventListener('mouseup', () => {
+  yellowBlurbs.style.backgroundColor = 'yellow';
+});
+
+const purpleBlurbs = document.querySelector('.destination p')
+purpleBlurbs.addEventListener('mousedown', () => {
+  purpleBlurbs.style.backgroundColor = 'purple';
+});
+
+//10 
+const introduction = document.querySelector('.intro');
+document.addEventListener('keydown', logKey);
+function logKey(e) {
+  console.log.textContent += ` ${e.code}`;
+};
+
+
+
+navigationLinks.addEventListener("click", function(event){
+  event.preventDefault()
+});
+
